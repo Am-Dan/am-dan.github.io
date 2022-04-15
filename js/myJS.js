39b0bf6900f835b162abc5e84d970f880129869c
Am-Dan <100517223+Am-Dan@users.noreply.github.com>
2022-04-15 17:54:39 +0300

    Закончил 9-10 урок по изучению js
    Выполнил задание по подсчёту стоимости заказа сайта

--- js/myJS.js
+++ js/myJS.js
@@ -1,4 +1,43 @@
 "use strict"
 
-let sprav = ("Какой сайт вы хотите? Справочник");
-console.log(sprav);
+let sprav = prompt("Какой сайт вы хотите? 1-Справочник. 2-Сайт Визитка. 3-Соц.Сеть. 4-Магазин");
+
+let disign = prompt("Каой дизайн? 1-Эконом. 2-Пристижный. 3-Самый лучший");
+
+let virst = prompt("Универсальность: 1-Только для ПК. 2-Только для смартфонов. 3-Универсал");
+
+if(sprav == 1){
+    sprav = 500;
+} 
+if(sprav == 2){
+    sprav = 550;
+}
+if(sprav == 3){
+    sprav = 600;
+}
+if(sprav == 4){
+    sprav = 700;
+}
+
+if(disign == 1){
+    disign = 200;
+}
+if(disign == 2){
+    disign = 500;
+}
+if(disign == 3){
+    disign = 1000;
+}
+
+if(virst == 1){
+    virst = 400;
+}
+if(virst == 2){
+    virst = 600;
+}
+if(virst == 3){
+    virst = 800;
+}
+
+let sum = sprav + disign + virst;
+alert(sum);
